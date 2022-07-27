affc430f6640a8f13910e5a7d24e7116adfc2923
abhishreepavan <72576507+abhishreepavan@users.noreply.github.com>
2022-07-27 10:06:32 +0530

    Add files via upload

---
+++ finaljob.groovy
@@ -0,0 +1,24 @@
+pipelineJob('aws') {
+  definition {
+    cps {
+      script(readFileFromWorkspace('pipeline jobs/ansible_pipeline_Jenkinsfile'))
+      sandbox()     
+    }
+  }
+}
+pipelineJob('buidling docker image and pushing it to ECR') {
+  definition {
+    cps {
+      script(readFileFromWorkspace('pipeline jobs/build_job_jenkinsfile'))
+      sandbox()     
+    }
+  }
+}
+pipelineJob('running a docker image') {
+  definition {
+    cps {
+      script(readFileFromWorkspace('pipeline jobs/deploy_job_Jenkinsfile'))
+      sandbox()     
+    }
+  }
+}
